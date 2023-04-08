45b6834c84791b213ffabe7af74256a8b37504f7
Esraa Mostfa <esraamostfa13@gmail.com>
2023-04-08 09:31:49 +0200

    Improve FakeDataSource

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,35 +4,39 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(private val reminders: MutableList<ReminderDTO>? = mutableListOf<ReminderDTO>()) : ReminderDataSource {
+class FakeDataSource(private val reminders: MutableList<ReminderDTO> = mutableListOf()) : ReminderDataSource {
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        reminders?.let {
-            return Result.Success(ArrayList(it))
+        return try {
+            Result.Success(reminders)
+        } catch (ex: Exception) {
+            Result.Error(ex.localizedMessage)
         }
-        return Result.Error(
-            "Reminders not found"
-        )
+
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminders?.add(reminder)
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-       val reminder = reminders?.first {
-            it.id == id
-        }
+        return try {
+            val reminder = reminders.firstOrNull {
+                it.id == id
+            }
+            if (reminder != null){
+                Result.Success(reminder)
+            } else{
+                Result.Error("Reminder not found!")
+            }
+        } catch (e: Exception) {
+            Result.Error(e.localizedMessage)
 
-        reminder?.let {
-            return Result.Success(it)
         }
-
-        return Result.Error("Reminder not found")
     }
 
     override suspend fun deleteAllReminders() {
-        reminders?.clear()
+        reminders.clear()
     }
 
 
